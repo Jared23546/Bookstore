eb872f6011c3e4a0c7ab899662fa0f337f6f8b87
Jared Christensen <90413445+Jared23546@users.noreply.github.com>
2022-02-16 13:10:08 -0700

    first

---
+++ Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+//using Bookstore.Models;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext context { get; set; }
+
+        public HomeController(BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var x = new BookViewModel
+            {
+                Books = context.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = context.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+
+            };
+            return View(x);
+        }
+        
+    }
+
+}
+
---
+++ Bookstore/Models/ViewModels/BookViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace Bookstore.Models.ViewModels
+{
+    public class BookViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
