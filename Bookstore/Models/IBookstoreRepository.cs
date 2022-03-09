7138ea75567fa3b6026bc58d51782fc5b65fb1d9
Jared Christensen <90413445+Jared23546@users.noreply.github.com>
2022-03-08 23:27:32 -0700

    10

--- Bookstore/Models/IBookstoreRepository.cs
+++ Bookstore/Models/IBookstoreRepository.cs
@@ -6,5 +6,13 @@ namespace Bookstore.Models
     public interface IBookstoreRepository
     {
         IQueryable<Book> Books { get; }
+
+        public void SaveBook(Book b);
+
+        public void CreateBook(Book b);
+
+        public void DeleteBook(Book b);
+
+
     }
 }
